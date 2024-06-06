b388aeced0cf029dd9230d20397c5443fc1b5b8c
ruwen <ruwen_lamm@web.de>
2024-06-06 08:23:06 +0200

    should create graph in out directory

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,6 @@ mod graph_algorithms;
 use crate::graph_algorithms::hp_alg::generate_random_undirected_graph;
 
 fn main() {
-    generate_random_undirected_graph(8000 ,5,100,false,"test").expect("error");
+    generate_random_undirected_graph(8000 ,5,100,false,"out/test").expect("error");
     //perform_hp_search("out/test",1).expect("error");
 }
